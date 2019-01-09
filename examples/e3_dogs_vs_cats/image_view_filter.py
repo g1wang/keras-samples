048c503f6298062b13689343f148a370c10b13af
kweilin123 <guilinw98@gmail.com>
2019-01-09 13:38:37 +0800

    image view

---
+++ examples/e3_dogs_vs_cats/image_view_filter.py
@@ -0,0 +1,39 @@
+from keras.applications import VGG16
+from keras import backend as K
+import numpy as np
+import matplotlib.pyplot as plt
+
+model = VGG16(weights='imagenet', include_top=False)
+layer_name = 'block5_conv1'
+filter_index = 3
+
+
+def depreocess_image(x):
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
+    x += 0.5
+    x = np.clip(x, 0, 1)
+    x *= 255
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+
+def generate_pattern(layer_name, filter_index, size=150):
+    layer_output = model.get_layer(layer_name).output
+    loss = K.mean(layer_output[:, :, :, filter_index])
+    grads = K.gradients(loss, model.input)[0]
+    grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+    iterate = K.function([model.input], [loss, grads])
+    input_img_data = np.random.random((1, size, size, 3)) * 20 + 128.
+    step = 1.
+    for i in range(40):
+        loss_value, grads_value = iterate([input_img_data])
+        input_img_data += grads_value * step
+
+    img = input_img_data[0]
+    return depreocess_image(img)
+
+
+plt.imshow(generate_pattern(layer_name, filter_index))
+plt.show()
